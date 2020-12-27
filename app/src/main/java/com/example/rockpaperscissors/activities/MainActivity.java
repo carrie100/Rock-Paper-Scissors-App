771b819673822f04a82f396e23a1b67a64da3b26
SA <mintedtech@gmail.com>
2020-12-27 16:10:16 -0500

    Done except for AutoSave and About/Info Text

--- app/src/main/java/com/example/rockpaperscissors/activities/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissors/activities/MainActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import com.example.rockpaperscissors.R;
 import com.example.rockpaperscissors.lib.Utils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -22,18 +24,40 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imgHuman, imgComputer;
+    private final String mCOMPUTER = "Computer", mPLAYER = "Player", mTIE = "Tie";
+
+    private ImageView mImageViewPlayer, mImageViewComputer;
     private View[] mRPSImageViews;
-    private TextView tvCurrentRoundWinner;
-    private TextView tvRound1Computer, tvRound1Player, tvRound1Winner;
-    private TextView tvRound2Computer, tvRound2Player, tvRound2Winner;
-    private TextView tvRound3Computer, tvRound3Player, tvRound3Winner;
+    private TextView mTvGameWinner;
 
     private TextView[] tvRoundsComputer, tvRoundsPlayer, tvRoundsWinner;
 
     private String[] mRPSStrings;
-    private int currentRound;
+    private int mCurrentRound;
     private int[] mRPSImagesIDs;
+    private int mCurrentRoundComputerPick, mCurrentRoundPlayerPick;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("CURRENT_ROUND", mCurrentRound);
+        outState.putInt("CURRENT_PLAYER_PICK", mCurrentRoundPlayerPick);
+        outState.putInt("CURRENT_COMPUTER_PICK", mCurrentRoundComputerPick);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mCurrentRound = savedInstanceState.getInt("CURRENT_ROUND");
+        mCurrentRoundPlayerPick = savedInstanceState.getInt("CURRENT_PLAYER_PICK");
+        mCurrentRoundComputerPick = savedInstanceState.getInt("CURRENT_COMPUTER_PICK");
+
+        if (mCurrentRoundPlayerPick !=-1)
+            mImageViewPlayer.setImageResource(mRPSImagesIDs[mCurrentRoundPlayerPick]);
+
+        if (mCurrentRoundComputerPick !=-1)
+            mImageViewComputer.setImageResource(mRPSImagesIDs[mCurrentRoundComputerPick]);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +68,8 @@ public class MainActivity extends AppCompatActivity {
 
         setupFields();
 
-        currentRound = 1;
         mRPSStrings = new String[]{"Rock", "Paper", "Scissors"};
+        startNewGame();
 
         setupFAB();
     }
@@ -63,8 +87,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupFields() {
-        imgHuman = findViewById(R.id.img_human_choice);
-        imgComputer = findViewById(R.id.img_computer_choice);
+        mImageViewPlayer = findViewById(R.id.img_player_choice);
+        mImageViewComputer = findViewById(R.id.img_computer_choice);
+        mTvGameWinner = findViewById(R.id.tv_board_winner);
 
         mRPSImageViews = new View[]{findViewById(R.id.image_rock),
                 findViewById(R.id.image_paper),
@@ -72,17 +97,15 @@ public class MainActivity extends AppCompatActivity {
         mRPSImagesIDs = new int[]
                 {R.drawable.rock, R.drawable.paper, R.drawable.scissors};
 
-        tvCurrentRoundWinner = findViewById(R.id.tv_board_winner);
-
-        tvRound1Computer = findViewById(R.id.tv_data_round1_computer);
-        tvRound1Player = findViewById(R.id.tv_data_round1_player);
-        tvRound1Winner = findViewById(R.id.tv_data_winner_round1);
-        tvRound2Computer = findViewById(R.id.tv_data_round2_computer);
-        tvRound2Player = findViewById(R.id.tv_data_round2_player);
-        tvRound2Winner = findViewById(R.id.tv_data_winner_round2);
-        tvRound3Computer = findViewById(R.id.tv_data_round3_computer);
-        tvRound3Player = findViewById(R.id.tv_data_round3_player);
-        tvRound3Winner = findViewById(R.id.tv_data_winner_round3);
+        TextView tvRound1Computer = findViewById(R.id.tv_data_round1_computer);
+        TextView tvRound1Player = findViewById(R.id.tv_data_round1_player);
+        TextView tvRound1Winner = findViewById(R.id.tv_data_winner_round1);
+        TextView tvRound2Computer = findViewById(R.id.tv_data_round2_computer);
+        TextView tvRound2Player = findViewById(R.id.tv_data_round2_player);
+        TextView tvRound2Winner = findViewById(R.id.tv_data_winner_round2);
+        TextView tvRound3Computer = findViewById(R.id.tv_data_round3_computer);
+        TextView tvRound3Player = findViewById(R.id.tv_data_round3_player);
+        TextView tvRound3Winner = findViewById(R.id.tv_data_winner_round3);
 
         tvRoundsComputer = new TextView[]{tvRound1Computer, tvRound2Computer, tvRound3Computer};
         tvRoundsPlayer = new TextView[]{tvRound1Player, tvRound2Player, tvRound3Player};
@@ -109,9 +132,32 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, Settings.class);
             startActivityForResult(intent, 1);
             return true;
+        } else if (id == R.id.action_new_game) {
+            startNewGame();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void startNewGame() {
+        // reset round to 1
+        mCurrentRound = 1;
+
+        // reset the game board
+        mImageViewComputer.setImageResource(0);
+        mImageViewPlayer.setImageResource(0);
+        mTvGameWinner.setText(R.string.welcome_to_a_new_game);
+
+        // reset the rounds board
+        for (int i = 0; i < tvRoundsWinner.length; i++) {
+            tvRoundsPlayer[i].setText("");
+            tvRoundsComputer[i].setText("");
+            tvRoundsWinner[i].setText("");
+        }
+
+        mCurrentRoundComputerPick = -1;
+        mCurrentRoundPlayerPick = -1;
     }
 
     @Override
@@ -124,13 +170,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void pickRPS(View view) {
-        int humanNumber = getHumanNumber(view);
-        updateHumanDrawable((ImageButton) view);
+        if (mCurrentRound <= 3) {
+            mCurrentRoundPlayerPick = getHumanNumber(view);
+            updateHumanDrawable((ImageButton) view);
 
-        int computerNumber = (int) Math.floor(Math.random() * 3);
-        updateComputerDrawable(computerNumber);
+            mCurrentRoundComputerPick = (int) Math.floor(Math.random() * 3);
+            updateComputerDrawable();
 
-        updateWinnerInfo(view, humanNumber, computerNumber);
+            updateWinnerInfo(view);
+        } else {
+            Snackbar.make(view, getString(R.string.game_already_over), Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     private int getHumanNumber(View view) {
@@ -143,77 +193,89 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateHumanDrawable(ImageButton view) {
-        ImageButton currentView = view;
-        Drawable currentDrawable = currentView.getDrawable();
-        imgHuman.setImageDrawable(currentDrawable);
+        Drawable currentDrawable = view.getDrawable();
+        mImageViewPlayer.setImageDrawable(currentDrawable);
 
-        int index = Integer.parseInt((String) currentView.getTag());
+        int index = Integer.parseInt((String) view.getTag());
         String rps = mRPSStrings[index];
-        TextView currentRoundHuman = tvRoundsPlayer[currentRound-1];
+        TextView currentRoundHuman = tvRoundsPlayer[mCurrentRound - 1];
 
         currentRoundHuman.setText(rps);
     }
 
-    private void updateComputerDrawable(int computerNumber) {
-        imgComputer.setImageResource(mRPSImagesIDs[computerNumber]);
-        String rps = mRPSStrings[computerNumber];
-        TextView currentRoundComputer = tvRoundsComputer[currentRound-1];
+    private void updateComputerDrawable() {
+        mImageViewComputer.setImageResource(mRPSImagesIDs[mCurrentRoundComputerPick]);
+        String rps = mRPSStrings[mCurrentRoundComputerPick];
+        TextView currentRoundComputer = tvRoundsComputer[mCurrentRound - 1];
 
         currentRoundComputer.setText(rps);
     }
 
-    private void updateWinnerInfo(View view, int humanNumber, int computerNumber) {
-
-        final String COMPUTER = "Computer", PLAYER = "Player";
-
+    private void updateWinnerInfo(View view) {
         String result, computerWins = "Computer won.", playerWins = "You won!", winner;
-        if (humanNumber == computerNumber) {
+        if (mCurrentRoundPlayerPick == mCurrentRoundComputerPick) {
             // display tie
-            result = "Tie Game!";
+            result = "Tie Game! Repeat this round";
             winner = "Tie";
-        } else if (humanNumber == 0 && computerNumber == 1) {
+        } else if (mCurrentRoundPlayerPick == 0 && mCurrentRoundComputerPick == 1) {
             // Rock vs Paper
             // Computer is winner
             result = computerWins;
-            winner = COMPUTER;
-        } else if (humanNumber == 0 && computerNumber == 2) {
+            winner = mCOMPUTER;
+        } else if (mCurrentRoundPlayerPick == 0 && mCurrentRoundComputerPick == 2) {
             // Rock vs Scissors
             // Human is the winner
             result = playerWins;
-            winner = PLAYER;
-        } else if (humanNumber == 1 && computerNumber == 0) {
+            winner = mPLAYER;
+        } else if (mCurrentRoundPlayerPick == 1 && mCurrentRoundComputerPick == 0) {
             // Paper vs Rock
             // Human is the winner
             result = playerWins;
-            winner = PLAYER;
-        } else if (humanNumber == 1 && computerNumber == 2) {
+            winner = mPLAYER;
+        } else if (mCurrentRoundPlayerPick == 1 && mCurrentRoundComputerPick == 2) {
             // Paper vs Scissors
             // Computer is winner
             result = computerWins;
-            winner = COMPUTER;
-        } else if (humanNumber == 2 && computerNumber == 0) {
+            winner = mCOMPUTER;
+        } else if (mCurrentRoundPlayerPick == 2 && mCurrentRoundComputerPick == 0) {
             // Scissors vs Rock
             // Computer is winner
             result = computerWins;
-            winner = COMPUTER;
-        } else if (humanNumber == 2 && computerNumber == 1) {
+            winner = mCOMPUTER;
+        } else if (mCurrentRoundPlayerPick == 2 && mCurrentRoundComputerPick == 1) {
             // Scissors vs Paper
             // Human is the winner
             result = playerWins;
-            winner = PLAYER;
+            winner = mPLAYER;
         } else {
             result = "Unknown";
             winner = result;
         }
 
-        // set main board text
-        tvCurrentRoundWinner.setText(result);
-
-        // set the winner in the score board
-        TextView currentRoundWinner = tvRoundsWinner[currentRound-1];
+        // set the round winner in the score board
+        TextView currentRoundWinner = tvRoundsWinner[mCurrentRound - 1];
         currentRoundWinner.setText(winner);
 
-        // go to next round
-        currentRound = currentRound == 3 ? 1 : currentRound + 1;
+        // Tell the user the results of this round
+        Snackbar.make(view,
+                "Round " + mCurrentRound + ": " + result, Snackbar.LENGTH_LONG).show();
+
+        // set main board text after all three rounds
+        if (mCurrentRound == 3 && !winner.equals(mTIE))
+            mTvGameWinner.setText(getString(R.string.game_winner_colon).concat(getGameWinner()));
+
+        // go to next round in current game
+        if (!winner.equals(mTIE))
+            mCurrentRound++;
+    }
+
+    private String getGameWinner() {
+        int computerWinCount = 0;
+        for (int i = 0; i < tvRoundsWinner.length; i++) {
+            computerWinCount = tvRoundsWinner[i].getText().toString().equals(mCOMPUTER)
+                    ? computerWinCount + 1 : computerWinCount;
+        }
+
+        return computerWinCount >=2 ? mCOMPUTER : mPLAYER;
     }
 }
